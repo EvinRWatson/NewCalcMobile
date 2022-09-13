8bf15d108a0a8b6c1dc3e33c3253ea7afbb29caa
Evin Watson <evinwatson@outlook.com>
2022-09-13 16:47:42 -0400

    Initial Commit

---
+++ NewCalcMobile/NewCalcMobile/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NewCalcMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        #region Page Initialization
+        public MainPage()
+        {
+            InitializeComponent();
+            WeightEntry.Text = App.Weight.ToString();
+            QuantityEntry.Text = App.Quantity.ToString();
+            REFEntry.Text = App.REF.ToString();
+            CurrentTotalNewLabel.Text = $"Current Total NEW: {App.TotalNEW}";
+            ElementNumLabel.Text = $"Element #{App.numElements}";
+        }
+
+        private async void ShowTotals(object sender, EventArgs e)
+        {
+            setTotalNEW();
+            await Navigation.PushAsync(new TotalsPage(), true);
+        }
+        #endregion
+
+        #region Set Global Variables
+        void SetWeight(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            string senderWeight = ((Entry)sender).Text;
+            if (senderWeight == null || senderWeight == "")
+                senderWeight = 0.ToString();
+            else
+                App.Weight = double.Parse(senderWeight);
+        }
+
+        void SetQuantity(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        { 
+            string senderQuantity = ((Entry)sender).Text;
+            if (senderQuantity == null || senderQuantity == "")
+                senderQuantity = 0.ToString();
+            else
+                App.Quantity = double.Parse(senderQuantity);
+        }
+
+        void SetREF(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            string senderREF = ((Entry)sender).Text;
+            if (senderREF == null || senderREF == "")
+                senderREF = 0.ToString();
+            else
+                App.REF = double.Parse(senderREF);
+        }
+
+        void SwitchSetIsGrain(object sender, ToggledEventArgs e)
+        {
+            bool senderIsGrain = e.Value;
+            App.isGrain = senderIsGrain;
+        }
+
+        #endregion
+
+        #region Functions
+        static void ResetInputVariables()
+        {
+            App.Weight = 0;
+            App.Quantity = 0;
+            App.REF = 0;
+            App.CurrentNEW = 0;
+            App.isGrain = false;
+        }
+
+
+        static void ResetTotalVariables()
+        {
+            App.numElements = 0;
+            App.TotalNEW = 0;
+        }
+
+        async void AddElement(object sender, EventArgs e)
+        {
+            setTotalNEW();
+            App.numElements++;
+            ResetInputVariables();
+            await Navigation.PushAsync(new MainPage(), true);
+        }
+
+        static double CalculateNEW()
+        {
+            App.CurrentNEW = App.Weight * App.Quantity * App.REF;
+            if (App.isGrain == true)
+                App.CurrentNEW /= 7000;
+            return App.CurrentNEW;
+        }
+
+        static void setTotalNEW()
+        {
+            App.TotalNEW += CalculateNEW();
+        }
+        #endregion
+    }
+}
---
+++ NewCalcMobile/NewCalcMobile/TotalsPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace NewCalcMobile
+{
+    public partial class TotalsPage : ContentPage
+    {
+        #region Variables
+        private static double CbrtTotalNEW { get; set; }
+        private static int StandoffWithCover { get; set; }
+        private static int StandoffWithoutCover { get; set; }
+        private static int StandoffOverpressure { get; set; }
+        private static int StandoffLightFrag { get; set; }
+        private static int StandoffHeavyFrag { get; set; }
+        #endregion
+
+        public TotalsPage()
+        {
+            InitializeComponent();
+            SetTotals();
+
+            NumElementLabel.Text = $"Element #            {App.numElements}";
+            TotalNEWLabel.Text = $"Total NEW:           {App.TotalNEW}";
+            wCoverLabel.Text = $"With Cover:          {StandoffWithCover}ft";
+            woCoverLabel.Text = $"Without Cover:    {StandoffWithoutCover}ft";
+            OverPressureLabel.Text = $"Overpressure:     {StandoffOverpressure}ft";
+            LightFragLabel.Text = $"Light Frag:           {StandoffLightFrag}ft";
+            HeavyFragLabel.Text = $"Heavy Frag:         {StandoffHeavyFrag}ft";
+        }
+
+        public void SetTotals()
+        {
+            if (App.TotalNEW != 0)
+            {
+                CbrtTotalNEW = CubeRoot(App.TotalNEW);
+                StandoffWithCover = ((int)(CbrtTotalNEW * 10)) + 1;
+                StandoffWithoutCover = ((int)(CbrtTotalNEW * 15)) + 1;
+                StandoffOverpressure = ((int)(CbrtTotalNEW * 20)) + 1;
+                StandoffLightFrag = ((int)(CbrtTotalNEW * 300)) + 1;
+                StandoffHeavyFrag = ((int)(CbrtTotalNEW * 500)) + 1;
+            }
+        }
+
+        public double CubeRoot(double input)
+        {
+            return Math.Pow(input, (1.0 / 3.0));
+        }
+
+        async void RestartApp(object sender, EventArgs e)
+        {
+            ResetTotals();
+            App.InitializeGlobals();
+            await Navigation.PushAsync(new MainPage(), true);
+        }
+
+        public static void ResetTotals()
+        {
+            CbrtTotalNEW = 0;
+            StandoffWithCover = 0;
+            StandoffWithoutCover = 0;
+            StandoffOverpressure = 0;
+            StandoffLightFrag = 0;
+            StandoffHeavyFrag = 0;
+        }
+    }
+}
